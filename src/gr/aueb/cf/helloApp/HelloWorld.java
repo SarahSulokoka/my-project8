04d9a43d99b0dd54b62bcf749ac66af72b32f8ad
Sara <sarahsulokoka@gmail.com>
2025-05-13 20:53:47 +0300

    feat: add hello world

---
+++ src/gr/aueb/cf/helloApp/HelloWorld.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.helloApp;
+
+/**
+ * This project created to show how
+ * to make a git project.
+ * And then how to stage and commit changes in local repo.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
